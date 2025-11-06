886ce83778dfa5475483a7a85b3162708a13fd77
Silverio Mirao <108777923+smirao@users.noreply.github.com>
2025-11-06 11:15:45 -0600

    Add files via upload

---
+++ brew-installs.sh
@@ -0,0 +1,43 @@
+brew install \
+  cmatrix \
+  cowsay \
+  docker \
+  gcc \
+  git \
+  go \
+  hashcat \
+  htop \
+  hydra \
+  lua \
+  maven \
+  mysql \
+  nasm \
+  ncurse \
+  neovim \
+  netcat \
+  nmap \
+  node \
+  numpy \
+  openvpn \
+  python \
+  qemu \
+  rust \
+  sherlock \
+  sketchybar \
+  skhd \
+  sqlite \
+  telnet \
+  tmux \
+  wget \
+  yabai
+
+brew install \
+  font-go-mono-nerd-font \
+  font-hack-nerd-font \
+  font-monaspice-nerd-font \
+  font-noto-sans-symbols-2 \
+  gcc-aarch64-embeded \
+  metasploit \
+  webtorrent \
+  wezterm \
+  wine-stable
